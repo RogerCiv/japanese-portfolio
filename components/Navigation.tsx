e0edbebf1db34ba4b686241c7262fea615e388cb
RogerCiv <rogercivdev@gmail.com>
2026-02-27 13:14:06 +0100

    style: Añade la clase `cursor-pointer` al componente `AnimatedThemeToggler`.

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -30,7 +30,7 @@ export default function Navigation() {
           {/* NavLinks y ThemeToggle */}
           <div className="flex items-center gap-4 md:gap-5">
             <NavLinks />
-            <AnimatedThemeToggler />
+            <AnimatedThemeToggler className="cursor-pointer" />
           </div>
         </div>
       </div>
